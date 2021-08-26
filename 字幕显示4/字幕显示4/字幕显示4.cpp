315e80f5b10104bcc1b53b2dd283bddea023878e
coderwang <wangyihuihn@163.com>
2021-08-27 00:25:07 +0800

    测试代码

---
+++ 字幕显示4/字幕显示4/字幕显示4.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<string.h>
+#include<windows.h>
+int main()
+{
+	int i, j;
+	char name[] = "wangyihui";
+	int name_len = strlen(name);
+	for (i = 0;i < name_len;i++)
+		printf(" ");
+	for (i = 1;i <= name_len;i++)
+	{
+		printf("\b");
+		for (j = 0;j < i;j++)
+			putchar(name[j]);
+		for (j = 0;j < i;j++)
+			printf("\b");
+		Sleep(500);
+	}
+	while (1)
+	{
+		for (i = 1;i <= name_len;i++)
+		{
+			printf("\r");
+			for (j = 0;j < name_len - i;j++)
+				putchar(name[j + i]);
+			printf(" ");
+			for (j = 0;j < i - 1;j++)
+				putchar(name[j]);
+			Sleep(500);
+		}
+	}
+	return 0;
+}
